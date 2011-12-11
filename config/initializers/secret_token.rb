05839a07d2aadcd95505186ee0b9ae09ed2b12db
Bashanta <bashanta@bashanta-laptop.(none)>
2011-12-11 21:58:57 +0545

    initial commit to the destination testing...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '16609ad878dff6c6b079bd9d13457b163a0f150844e96d0a603ed1680f5c0ee20a9ca5ff30c224fc3c0974431f6b15f2eebfee5ba8cc95852aac737a14697ec4'
